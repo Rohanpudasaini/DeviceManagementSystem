ee4b2812212f5bd54df94a2276325c2f81606563
Rohan <pudasainirohan0@gmail.com>
2024-04-15 11:27:51 +0545

    refactor:remove unused code and cleaning

--- auth/permission_checker.py
+++ auth/permission_checker.py
@@ -18,20 +18,3 @@ class PermissionChecker:
                 status_code=403,
                 detail="Not enough permissions to access this resource")
         return user
-
-# class ContainPermission:
-#     def __init__(self, permissions_required: list):
-#         self.permissions_required = permissions_required
-
-#     def __call__(self, user:dict = Depends(auth.validate_token)):
-#         for permission_required in self.permissions_required:
-#             print(permission_required)
-#             # print(Role.get_role_permissions(user['role']))
-#             is_permitted = Role.role_got_permission(permission_required,user['user_identifier'])            
-#             # if permission_required not in Role.get_role_permissions(user['role']):
-#             if not is_permitted:
-#                 # raise HTTPException(
-#                 #     status_code=403,
-#                 #     detail="Not enough permissions to access this resource")
-#                 return False
-#         return True
