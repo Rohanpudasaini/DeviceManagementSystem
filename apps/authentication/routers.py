a1908f6917c197bbb8f9073e05d5992e1a3cf8f9
Rohan <pudasainirohan0@gmail.com>
2024-04-29 13:52:52 +0545

    docs: Added doc string to only some class to check how it looks

--- apps/authentication/routers.py
+++ apps/authentication/routers.py
@@ -30,12 +30,51 @@ async def login(
     data: LoginModel,
     session=Depends(get_session),
 ):
+    """
+    Authenticates a user based on the provided email and password encapsulated within `data` 
+    from the request body. This asynchronous endpoint handles user login, validating the 
+    credentials and returning the appropriate response.
+
+    This function leverages FastAPI's dependency injection system to handle the database session, 
+    and expects the user's login details to be passed as a JSON object that conforms to the 
+    `LoginModel` schema.
+
+    Parameters:
+    - data (LoginModel): An object containing the user's email and password, received from the request body.
+    - session (Session): A database session provided by FastAPI's dependency injection system. This session
+      is used to handle all database transactions involved in the authentication process.
+
+    Returns:
+    - dict: A dictionary representing the authentication status of the user, which could include user information 
+      and authentication tokens if the login is successful, or error messages if it is not.
+
+    Raises:
+    - HTTPException: An error response with an appropriate status code and message if the authentication fails 
+      for reasons such as incorrect credentials or server issues.
+    """
     user_object = User.from_email(session, data.email)
     return User.login(session, user_object, **data.model_dump(exclude_unset=True))
 
 
 @router.post("/login/refresh-token", tags=["Authentication"], status_code=201)
 async def get_new_access_token(data: RefreshTokenModel):
+    """
+    Generates a new access token from a provided refresh token.
+
+    This asynchronous endpoint receives a refresh token encapsulated within `data` from the request body
+    and attempts to decode it to issue a new access token. If the refresh token is valid, a new access token
+    is returned in the response. If the token is invalid or expired, it raises an HTTPException with status code 401.
+
+    Parameters:
+    - data (RefreshTokenModel): An object containing the `token` field with the refresh token, received from the request body.
+
+    Returns:
+    - dict: A dictionary containing the new `access_token` if the refresh token is successfully decoded.
+
+    Raises:
+    - HTTPException: An error response with status code 401 and detailed message if the refresh token
+      is invalid or fails verification.
+    """
     token = auth.decode_refresh_jwt(data.token)
     if token:
         return response_model(data={"access_token": token})
@@ -53,6 +92,26 @@ async def update_password(
     token=Depends(auth.validate_token),
     session=Depends(get_session),
 ):
+    """
+    Updates a user's password if the old password matches and the new password is different.
+
+    This endpoint processes a password change request after validating the user's current (old) password
+    and ensuring the new password is not the same as the current one. The user is identified using a token,
+    which must be valid and active. If the old password doesn't match or if the new password is the same
+    as the old, an HTTPException is raised with a status code of 409.
+
+    Parameters:
+    - data (ChangePasswordModel): An object containing the old and new passwords from the request body.
+    - token (dict): A dictionary extracted from the user's access token via dependency injection.
+    - session (Session): A database session to handle transaction with the database.
+
+    Returns:
+    - dict: A dictionary containing a message about the successful password update.
+
+    Raises:
+    - HTTPException: An error response with status code 409 indicating either mismatch of old password
+      or if the new password is the same as the current password. The detailed error message will be provided.
+    """
     user_to_update = User.from_email(session, token.get("user_identifier"))
     if not auth.verify_password(data.old_password, user_to_update.password):
         logger.warning("Password don't match")
@@ -84,6 +143,27 @@ async def reset_password(
     confirm_password=Form(),
     session=Depends(get_session),
 ):
+    """
+    Resets a user's password given a valid OTP (One-Time Password) token and matching new passwords.
+
+    This endpoint handles password reset requests where the user has already verified their identity
+    through an OTP sent to their registered email. The password reset process involves validating
+    the OTP token, checking the match between the new password and its confirmation, and updating
+    the password in the database.
+
+    Parameters:
+    - token (str): The OTP token provided by the user, used to validate their identity.
+    - new_password (str): The new password the user wishes to set.
+    - confirm_password (str): A confirmation of the new password for validation purposes.
+    - session (Session): A database session to handle transactions with the database.
+
+    Returns:
+    - dict: A dictionary containing a success message indicating that the password has been updated.
+
+    Raises:
+    - HTTPException: An error response with status code 400 if the new password and confirm password do not match,
+      or other status codes as appropriate for other failure conditions (e.g., invalid token).
+    """
     if new_password != confirm_password:
         raise HTTPException(
             status_code=400,
@@ -105,6 +185,25 @@ async def forget_password(
     backgroundTasks: BackgroundTasks,
     session=Depends(get_session),
 ):
+    """
+    Initiates a password reset process by sending a temporary password to the user's email.
+
+    This endpoint generates and sends a new temporary password to the user specified by the email
+    in the `ResetPasswordModel`. It verifies the existence of the user in the database, creates
+    a new hashed temporary password, and sends it via email asynchronously. The temporary password
+    and its creation time are updated in the database for further verification during the login process.
+
+    Parameters:
+    - data (ResetPasswordModel): The model containing the email of the user requesting a password reset.
+    - backgroundTasks (BackgroundTasks): Task manager for running operations in the background.
+    - session (Session): Database session for handling transactions.
+
+    Returns:
+    - dict: A dictionary containing a success message instructing the user to check their email for the temporary password.
+
+    Raises:
+    - HTTPException: An error response with status code 404 if no user is found with the given email.
+    """
     user_object = User.from_email(session, data.email)
     if not user_object:
         raise HTTPException(
